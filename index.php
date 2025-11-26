9374ceacc2893eb9a4e0e9e89a0fa58af35d6707
putr894 <lagibacaemail@gmail.com>
2025-11-26 21:16:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta obcaecati beatae maxime quasi consequuntur corporis libero harum porro, blanditiis, a cum accusamus minus nihil enim vel dignissimos. Tenetur, magnam perspiciatis!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+</ul>
 </body>
 </html>
